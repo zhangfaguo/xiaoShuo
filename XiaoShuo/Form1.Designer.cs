fa5faceaeeeb21a64b73f164b429d779261af281
zhangfaguo <zhangfaguo98@163.com>
2016-12-13 12:56:13 +0800

    add region formatter

--- XiaoShuo/Form1.Designer.cs
+++ XiaoShuo/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -46,15 +48,37 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "格式化区域数量";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(410, 322);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,6 +86,8 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- XiaoShuo/Form1.cs
+++ XiaoShuo/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
 namespace XiaoShuo
 {
     public partial class Form1 : Form
@@ -56,5 +58,106 @@ namespace XiaoShuo
 
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var file = openFileDialog1.FileName;
+                var txtContent = string.Empty;
+                using (var sr = new StreamReader(file, Encoding.GetEncoding("gb2312")))
+                {
+                    txtContent = sr.ReadToEnd();
+                }
+                if (!string.IsNullOrEmpty(txtContent))
+                {
+                    var ms = Regex.Matches(txtContent, @"(?<code>\d+)\s+(?<name>\w+)");
+                    if (ms.Count > 0)
+                    {
+                        var list = new List<Area>();
+
+                        foreach (Match item in ms)
+                        {
+                            list.Add(new Area()
+                            {
+                                Code = item.Groups["code"].Value,
+                                Name = item.Groups["name"].Value
+                            });
+                        }
+
+                        if (list.Count > 0)
+                        {
+                            var jsonData = new List<Area>();
+                            #region 获取区域
+                            var topList = list.Where(c => c.Code.EndsWith("0000"));
+                            var temp = new List<Area>();
+                            jsonData.AddRange(topList);
+
+                            foreach (var item in jsonData)
+                            {
+                                InitChild(item, list);
+                            }
+                            if (jsonData.Count > 0)
+                            {
+                                var str = JsonConvert.SerializeObject(jsonData);
+                                using (var sw = new StreamWriter(@"d:\\json.txt", true, Encoding.Default))
+                                {
+                                    sw.Write(str);
+                                }
+                            }
+                            #endregion
+                        }
+                    }
+                }
+            }
+        }
+
+        private void InitChild(Area top, List<Area> data)
+        {
+            var code = top.Code;
+            var arr = code.ToArray();
+            var index = 0;
+            var len = arr.Count();
+            for (var i = len - 1; i >= 0; i--)
+            {
+                if (arr[i] !='0')
+                {
+                    index = i;
+                    break;
+                }
+            }
+            var temp = "";
+            for (var i = 0; i <= index; i++)
+            {
+                temp += arr[i];
+            }
+            temp += "(?<num>\\d{2})";
+
+            for (var i = 5; i > index + 2; i--)
+            {
+                temp += "0";
+            }
+
+            if (index != 5)
+            {
+                var list = data.Where(c => Regex.Match(c.Code, temp).Success && c.Code != code).ToList();
+                top.Childs = list;
+
+                foreach (var item in top.Childs)
+                {
+                    InitChild(item, data);
+                }
+            }
+        }
+
+        private class Area 
+        {
+            public string Code { get; set; }
+
+            public string Name { get; set; }
+            public IList<Area> Childs { get; set; }
+        }
+
+     
     }
 }
